5b43b810c69a81c96962464b06b0ef1e147a2194
geniusrks1 <gmrohitkumar@gmail.com>
2023-07-13 15:03:32 +0530

    mailers done

--- models/friendship.js
+++ models/friendship.js
@@ -1,23 +1,20 @@
 const mongoose = require('mongoose');
 
-const friendshipSchema = new mongoose.Schema
-(
-    {
-        from_user:
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        to_user:
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        }
+
+const friendshipSchema = new mongoose.Schema({
+    // the user who sent this request
+    from_user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     },
-    {
-        timestamps: true
-    }
-);
+    // the user who accepted this request, the naming is just to understand, otherwise, the users won't see a difference
+    to_user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+},{
+    timestamps: true
+});
 
 const Friendship = mongoose.model('Friendship', friendshipSchema);
 module.exports = Friendship;
--- models/like.js
+++ models/like.js
@@ -1,31 +1,25 @@
 const mongoose = require('mongoose');
 
-const likeSchema = mongoose.Schema
-(
-    {
-        user:
-        {
-            type: mongoose.Schema.ObjectId
-        },
-        // this defines the object id of the liked object
-        likeable:
-        {
-            type: mongoose.Schema.ObjectId,
-            required: true,
-            refPath: 'onModel'
-        },
-        // this field is used for defining the type of the object since this is a dynamic reference
-        onModel:
-        {
-            type: String,
-            required: true,
-            enum: ['Post', 'Comment']
-        }
+
+const likeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.ObjectId
     },
-    {
-        timestamps: true
+    // this defines the object id of the liked object
+    likeable: {
+        type: mongoose.Schema.ObjectId,
+        require: true,
+        refPath: 'onModel'
+    },
+    // this field is used for defining the type of the liked object since this is a dynamic reference
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Post', 'Comment']
     }
-);
+}, {
+    timestamps: true
+});
 
 
 const Like = mongoose.model('Like', likeSchema);
